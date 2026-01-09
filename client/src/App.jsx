b502dace60b2bad1abcfa794a3f7e1bdad099d45
vikramdev772 <vikramdev772@gmail.com>
2026-01-09 15:14:21 +0530

    loader component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import "./App.css"
-import { Route, Routes } from 'react-router-dom'
+import React from "react";
+import "./App.css";
+import { Route, Routes,BrowserRouter } from "react-router-dom";
+import L from "./ui/L";
 function App() {
   return (
     <>
-    <Router>
-
-      <Routes>
-        <Route path="/" element={}/>
-      </Routes>
-    </Router>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<L />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
