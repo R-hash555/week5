cd88e35fbb9a1fad063db62e3498dee4d762f590
vikramdev772 <vikramdev772@gmail.com>
2026-01-23 15:04:06 +0530

    Login pending

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { Route, Routes,BrowserRouter } from "react-router-dom";
 import L from "./ui/L";
 import Signup from "./auth/Signup";
+import Login from "./auth/Login";
 function App() {
   return (
     <>
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<L />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </>
